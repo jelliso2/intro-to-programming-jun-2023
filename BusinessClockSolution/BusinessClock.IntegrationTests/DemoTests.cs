3ed21fcca78799da36fb0c15d38c596a3443e22b
Josh Ellison <jelliso2@uccs.edu>
2023-06-19 15:24:43 -0400

    before break code

---
+++ BusinessClockSolution/BusinessClock.IntegrationTests/DemoTests.cs
@@ -0,0 +1,27 @@
+﻿
+namespace BusinessClock.IntegrationTests;
+
+public class DemoTests
+{
+    [Fact]
+    public void CanAddTwoNumbers()
+    {
+        //Given
+        int a = 10, b = 20, answer;
+        //When
+        answer = a + b; // Systtem under test
+        // Then
+        Assert.Equal(30, answer);
+    }
+
+    [Theory]
+    [InlineData(10, 20,30)]
+
+    public void CanAddAnyTwoNumbers(int a, int b,int expected)
+    {
+        int answer = a + b;
+
+        Assert.Equal(expected, answer);
+    }
+
+}
