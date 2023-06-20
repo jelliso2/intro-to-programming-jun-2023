c7acac085e9d98f7cdb740f709c676b415a61e2b
Josh Ellison <jelliso2@uccs.edu>
2023-06-20 13:49:36 -0400

    before lunch

---
+++ BasicConsoleApp3Solution/BasicConsoleApp3/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+using GeneralUtilities;
+
+Console.WriteLine("Enter your first name");
+string firstName = Console.ReadLine();
+
+Console.WriteLine("Enter your last name");
+string lastName = Console.ReadLine();
+
+
+string fullname = Formatters.FormatName(firstName, lastName);
+
+
+Console.WriteLine($"Hello, {fullname}");
