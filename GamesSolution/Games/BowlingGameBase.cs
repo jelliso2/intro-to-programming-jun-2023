5326990476aa9cd1d96250e44f79c125709b2156
Josh Ellison <jelliso2@uccs.edu>
2023-06-21 16:34:47 -0400

    end of day

---
+++ GamesSolution/Games/BowlingGameBase.cs
@@ -0,0 +1,38 @@
+﻿namespace Games
+{
+    public abstract class BowlingGameBase
+    {
+        private readonly List<Player> _players = new();
+        protected abstract void GuardForValidScore(int score);
+
+        public void AddPlayer(string name, int score)
+        {
+            GuardForValidScore(score);
+            GuardForPlayerExisting(name);
+
+            _players.Add(new Player(name, score));
+
+            // store some kind of list of players and their scores
+            // unless a player with that same name already exists.
+            // in that case, punch them in the nose.
+        }
+
+        internal List<Player> GetPlayers()
+        {
+            return _players;
+        }
+
+        private void GuardForPlayerExisting(string name)
+        {
+            if (PlayerExists(name))
+            {
+                throw new PlayerAlreadyAddedToGameException();
+            }
+        }
+
+        private bool PlayerExists(string name)
+        {
+            return _players.Any(p => p.Name.Trim().ToLowerInvariant() == name.Trim().ToLowerInvariant());
+        }
+    }
+}
