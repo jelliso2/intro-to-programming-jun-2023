77ed64afaf6646152c0c40ad18c118cd695c3d36
Josh Ellison <jelliso2@uccs.edu>
2023-06-22 16:52:58 -0400

    stringCalcThursday

---
+++ stringcalculatorThursday/stringcalculator-master/StringCalculator.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestPlatform.Common.Exceptions;
+using System.Security.AccessControl;
+
+namespace StringCalculator;
+
+
+
+public class StringCalculator
+{
+
+
+    private readonly ILogger _logger;
+
+    public StringCalculator(ILogger logger)
+    {
+        _logger = logger;
+    }
+
+    public int Add(string numbers)
+    {
+        int result = 0;
+        if (numbers == "") { return 0; }
+        else
+        {
+            result = numbers.Split(',', '\n').Select(int.Parse).Sum();
+
+        }
+
+        _logger.Write(result.ToString());
+        return result;
+    }
+}
+
+public interface ILogger
+{
+    void Write(string message);
+}
