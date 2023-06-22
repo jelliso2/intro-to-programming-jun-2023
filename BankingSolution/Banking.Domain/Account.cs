e3205f5bfb68f64f54fbc5812f0addab087a8d34
Josh Ellison <jelliso2@uccs.edu>
2023-06-22 11:42:12 -0400

    done with this version

--- BankingSolution/Banking.Domain/Account.cs
+++ BankingSolution/Banking.Domain/Account.cs
@@ -1,25 +1,34 @@
-﻿namespace Banking.Domain
+﻿namespace Banking.Domain;
+
+public enum LoyaltyLevel { Standard,Gold}
+public class Account
 {
-    public class Account
+    private decimal _balance = 5000;
+    public LoyaltyLevel AccountType { get; set; } = LoyaltyLevel.Standard;
+    public void Deposit(decimal amountToDeposit)
     {
-        private decimal _balance = 5000;
-        public void Deposit(decimal amountToDeposit)
+        if(AccountType == LoyaltyLevel.Gold)
+        {
+            _balance += amountToDeposit* 1.10M;
+        }
+        else
         {
             _balance += amountToDeposit;
         }
+        
+    }
 
-        public decimal GetBalance()
-        {
-            return _balance;
-        }
+    public decimal GetBalance()
+    {
+        return _balance;
+    }
 
-        public void Withdrawal(decimal amountToWithdrawal)
+    public void Withdrawal(decimal amountToWithdrawal)
+    {
+        if(amountToWithdrawal >= _balance)
         {
-            if(amountToWithdrawal >= _balance)
-            {
-                throw new OverdraftException();
-            }
-            _balance -= amountToWithdrawal;
+            throw new OverdraftException();
         }
+        _balance -= amountToWithdrawal;
     }
 }
--- BankingSolution/Banking.UnitTests/BankAccount/GoldCustomerGetsABonusOnDeposits.cs
+++ BankingSolution/Banking.UnitTests/BankAccount/GoldCustomerGetsABonusOnDeposits.cs
@@ -4,10 +4,20 @@ namespace Banking.UnitTests.BankAccount;
 
 public class GoldCustomerGetsABonusOnDeposits
 {
-    [Fact(Skip = "Working on it")]
+    [Fact]
     public void BonusIsApplied()
     {
+        var account = new Account();
+        var openingBalance = account.GetBalance();
+        account.AccountType = LoyaltyLevel.Gold;
+        var amountToDeposits = 100M;
 
+        var expectedNewBalance = openingBalance + amountToDeposits + 10M;
+
+
+        account.Deposit(amountToDeposits);
+
+        Assert.Equal(expectedNewBalance, account.GetBalance());
     }
         
 }
